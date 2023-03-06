f5d970a657ae21360bcf8ef4ff753110175c6b13
longshort <tbvjqkdlcm1@naver.com>
2023-03-07 00:32:08 +0900

    404 error modify

--- src/main/java/com/scheduler/domain/post/PostController.java
+++ src/main/java/com/scheduler/domain/post/PostController.java
@@ -1,6 +1,7 @@
 package com.scheduler.domain.post;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
